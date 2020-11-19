f70cc4021a1bd0063ecd7b88138faaf97cdce595
dukhyunko93 <dukhyunko93@gmail.com>
2020-11-19 12:31:09 -0500

    style change

--- js/main.js
+++ js/main.js
@@ -52,8 +52,8 @@
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				$('html, body').animate({
-					scrollTop: (target.offset().top - navHeight + 70)
-				}, 1000, "easeInOutExpo");
+					scrollTop: (target.offset().top - (navHeight/2))
+				}, 800, "easeInOutExpo");
 				return false;
 			}
 		}
